972d83e16d44a1ec7e4277707f1c00183271e566
unknown <kumaranand8126@gmail.com>
2018-10-30 14:28:45 +0530

    Recycler view added

--- app/src/main/java/com/teammanagementapp/ankush/memory/GamePlay.java
+++ app/src/main/java/com/teammanagementapp/ankush/memory/GamePlay.java
@@ -2,24 +2,33 @@ package com.teammanagementapp.ankush.memory;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class GamePlay extends AppCompatActivity {
+public class GamePlay extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
 
 
-    RecyclerView recyclerView;
     TextView textView;
+    MyRecyclerViewAdapter adapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gameplay);
-        recyclerView= findViewById(R.id.recycler_view);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        String[] data = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"};
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.rvNumbers);
+        int numberOfColumns = 6;
+        recyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
+        adapter = new MyRecyclerViewAdapter(this, data);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
 
 
 
@@ -27,7 +36,8 @@ public class GamePlay extends AppCompatActivity {
     }
 
 
-
-
-
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText( this, "You clicked number " + adapter.getItem(position) + ", which is at cell position " + position, Toast.LENGTH_SHORT ).show();
+    }
 }
